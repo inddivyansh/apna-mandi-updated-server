5ed559e5513e4e9da309d92438818f01c0a6de9f
Manorath <chughmanorath@gmail.com>
2025-07-29 19:53:22 +0530

    done

--- server.js
+++ server.js
@@ -9,12 +9,10 @@ import authRoutes from './routes/authRoutes.js';
 import buyerRoutes from './routes/buyerRoutes.js';
 import sellerRoutes from './routes/sellerRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
-<<<<<<< HEAD
 import chatbotRoutes from './routes/chatbotRoutes.js';
-=======
 import requirementRoutes from './routes/requirement.js';
 
->>>>>>> check
+
 
 dotenv.config();
 connectDB();
@@ -29,11 +27,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/buyer', buyerRoutes);
 app.use('/api/seller', sellerRoutes);
 app.use('/api/admin', adminRoutes);
-<<<<<<< HEAD
+
 app.use('/api/chatbot', chatbotRoutes);
 
-=======
 app.use('/api',requirementRoutes)
->>>>>>> check
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
